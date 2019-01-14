c3d0f24dcf7ce0385aff1ca535356068068ad814
Laksh Bhambhani <lakshbh@gmail.com>
2019-01-13 19:06:52 -0800

    Update CubeMaker.java

--- RubixsCubeScanner/CubeMaker.java
+++ RubixsCubeScanner/CubeMaker.java
@@ -1,4 +1,6 @@
 import java.awt.Color;
+import java.util.Scanner;
+import java.util.List;
 import java.awt.Graphics2D;
 import java.awt.Point;
 import java.awt.image.BufferedImage;
@@ -7,113 +9,109 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
 import java.nio.charset.Charset;
 import java.util.ArrayList;
+import java.util.Arrays;
+
 import javax.imageio.ImageIO;
 import javax.swing.Box.Filler;
 import javax.swing.JFileChooser;
 import javax.swing.filechooser.FileNameExtensionFilter;
-
-/*
- * Docs: 
- * 
- * 
- * 
- * sides: 1010, 1175, 1340, 1505, 1670, 1835
- */
+import java.io.BufferedReader; 
+import java.io.FileReader; 
+import java.io.IOException; 
+import java.nio.charset.StandardCharsets; 
+import java.nio.file.Files; 
+import java.nio.file.Paths; 
+import java.util.ArrayList; 
+import java.util.Collections; 
+import java.util.List;
+import java.util.StringTokenizer;
 
 public class CubeMaker {
 
-	private static Color color;
-	private StringToColor stringToColor = new StringToColor();
+	private Color color;
 
-	private int[] side1X = { 185, 350, 350, 185, 20, 20, 20, 185, 350 };
-	private int[] side1Y = { 675, 675, 840, 840, 840, 675, 510, 510, 510 };
+	private int[] side1X = {185, 350, 350, 185, 20, 20, 20, 185, 350};
+	private int[] side1Y = {675, 675, 840, 840, 840, 675, 510, 510, 510};
 
-	private int[] side2X = { 680, 845, 845, 680, 515, 515, 515, 680, 845 };
-	private int[] side2Y = { 675, 675, 840, 840, 840, 675, 510, 510, 510 };
+	private int[] side2X = {680, 845, 845, 680, 515, 515, 515, 680, 845};
+	private int[] side2Y = {675, 675, 840, 840, 840, 675, 510, 510, 510};
 
-	private int[] side3X = { 1175, 1340, 1340, 1175, 1010, 1010, 1010, 1175, 1340 };
-	private int[] side3Y = { 675, 675, 840, 840, 840, 675, 510, 510, 510 };
+	private int[] side3X = {1175, 1340, 1340, 1175, 1010, 1010, 1010, 1175, 1340};
+	private int[] side3Y = {675, 675, 840, 840, 840, 675, 510, 510, 510};
 
-	private int[] side4X = { 1670, 1835, 1835, 1670, 1505, 1505, 1505, 1670, 1835 };
-	private int[] side4Y = { 675, 675, 840, 840, 840, 675, 510, 510, 510 };
+	private int[] side4X = {1670, 1835, 1835, 1670, 1505, 1505, 1505, 1670, 1835};
+	private int[] side4Y = {675, 675, 840, 840, 840, 675, 510, 510, 510};
 
-	private int[] side5X = { 680, 845, 845, 680, 515, 515, 515, 680, 845 };
-	private int[] side5Y = { 180, 180, 345, 345, 345, 180, 15, 15, 15 };
+	private int[] side5X = {680, 845, 845, 680, 515, 515, 515, 680, 845};
+	private int[] side5Y = {180, 180, 345, 345, 345, 180, 15, 15, 15};
 
-	private int[] side6X = { 680, 845, 845, 680, 515, 515, 515, 680, 845 };
-	private int[] side6Y = { 1175, 1175, 1340, 1340, 1340, 1175, 1010, 1010, 1010 };
+	private int[] side6X = {680, 845, 845, 680, 515, 515, 515, 680, 845};
+	private int[] side6Y = {1175, 1175, 1340, 1340, 1340, 1175, 1010, 1010, 1010};
 
-
-	public CubeMaker(String[] side1, String[] side2, String[] side3, String[] side4, String[] side5, String[] side6)
-			throws IOException {
+	public static void main(String[] args) throws IOException {
+		
+		StringToColor stringToColor = new StringToColor();
+		StringToInt StringToInt = new StringToInt();
+		CubeMaker object = new CubeMaker();
+		
 		BufferedImage img = null;
 		try {
 			img = ImageIO.read(new File("Cube.png"));
 		} catch (IOException e) {
 		}
+		
 		Graphics2D g2d = img.createGraphics();
 
-		// side1
+		List<String> lines = Collections.emptyList(); 
+		String data;
+		
+		try { lines = Files.readAllLines(Paths.get("data.txt"), StandardCharsets.UTF_8); } 
+		catch (IOException e) {  
+			e.printStackTrace(); } 
+		
+		List<String> result = new ArrayList<>();
+		
+		try (FileReader f = new FileReader("data.txt")) {
+		    StringBuffer sb = new StringBuffer();
+		    while (f.ready()) {
+		        char c = (char) f.read();
+		        if (c == ',') {
+		            result.add(sb.toString());
+		            sb = new StringBuffer();
+		        } else {
+		            sb.append(c);
+		        }
+		    }
+		    if (sb.length() > 0) {
+		        result.add(sb.toString());
+		    }
+		}       
+		System.out.println("Result:");
+		System.out.println(result);
+		System.out.println("Size: " + result.size());
+		
+		File outputfile = new File("ModelledCube.png");
+		String colour;
+		
 		for (int i = 0; i < 9; i++) {
-			String colour = side1[i];
-			color = stringToColor.StringToColor(colour);
+			
+			colour = result.get(i).toString();
+			System.out.println(colour);
+			String num = StringToInt.StringToInt(colour);
+			Color color = stringToColor.StringToColor(colour); //Color.decode(num);//
+			System.out.println(color);
 			g2d.setColor(color);
-			g2d.fillRect(side1X[i], side1Y[i], 145, 145);
+			g2d.fillRect(object.side1X[i], object.side1Y[i], 145, 145);
+			
 		}
-
-		// side2
-		for (int i = 0; i < 9; i++) {
-			String colour = side2[i];
-			color = stringToColor.StringToColor(colour);
-			g2d.setColor(color);
-			g2d.fillRect(side2X[i], side2Y[i], 145, 145);
-		}
-
-		// side3
-		for (int i = 0; i < 9; i++) {
-			String colour = side3[i];
-			color = stringToColor.StringToColor(colour);
-			g2d.setColor(color);
-			g2d.fillRect(side3X[i], side3Y[i], 145, 145);
-		}
-
-		// side4
-		for (int i = 0; i < 9; i++) {
-			String colour = side4[i];
-			color = stringToColor.StringToColor(colour);
-			g2d.setColor(color);
-			g2d.fillRect(side4X[i], side4Y[i], 145, 145);
-		}
-
-		// side5
-		for (int i = 0; i < 9; i++) {
-			String colour = side5[i];
-			color = stringToColor.StringToColor(colour);
-			g2d.setColor(color);
-			g2d.fillRect(side5X[i], side5Y[i], 145, 145);
-		}
-
-		// side6
-		for (int i = 0; i < 9; i++) {
-			String colour = side6[i];
-			color = stringToColor.StringToColor(colour);
-			g2d.setColor(color);
-			g2d.fillRect(side6X[i], side6Y[i], 145, 145);
-		}
-
-		g2d.setColor(Color.RED);
-		g2d.fillRect(1835, 520, 145, 145);
-		File outputfile = new File("saved.png");
+	
 		ImageIO.write(img, "png", outputfile);
-		System.out.println("Successfull");
+		System.out.println("Successfully made. Find your file in the same directory as this project.");
 
-	}
-
-	public static void main(String[] args) throws IOException {
-		// CubeMaker object2 = new CubeMaker();
-
+		
 	}
 }
